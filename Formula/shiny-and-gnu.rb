095e3d08fa1258054ccc07ed322382e162d026f2
Andrea Falco <andrea@falco.sh>
2020-01-05 18:55:30 +0100

    Dependency recommended, easily skippable

--- Formula/shiny-and-gnu.rb
+++ Formula/shiny-and-gnu.rb
@@ -8,28 +8,28 @@ class ShinyAndGnu < Formula
   option "with-make", "Known to cause problems, see `brew info make`"
   option "with-unzip", "Known to cause problems, see `brew info unzip`"
 
-  depends_on "bash"
-  depends_on "coreutils"
-  depends_on "diffutils"
-  depends_on "findutils"
-  depends_on "gawk"
-  depends_on "gnu-indent"
-  depends_on "gnu-sed"
-  depends_on "gnu-tar"
-  depends_on "gnu-which"
-  depends_on "gnutls"
-  depends_on "gpatch"
-  depends_on "grep"
-  depends_on "gzip"
-  depends_on "less"
-  depends_on "openssh"
-  depends_on "python"
-  depends_on "rsync"
-  depends_on "vim"
-  depends_on "watch"
-  depends_on "wdiff"
-  depends_on "wget"
-  depends_on "zsh"
+  depends_on "bash" => :recommended
+  depends_on "coreutils" => :recommended
+  depends_on "diffutils" => :recommended
+  depends_on "findutils" => :recommended
+  depends_on "gawk" => :recommended
+  depends_on "gnu-indent" => :recommended
+  depends_on "gnu-sed" => :recommended
+  depends_on "gnu-tar" => :recommended
+  depends_on "gnu-which" => :recommended
+  depends_on "gnutls" => :recommended
+  depends_on "gpatch" => :recommended
+  depends_on "grep" => :recommended
+  depends_on "gzip" => :recommended
+  depends_on "less" => :recommended
+  depends_on "openssh" => :recommended
+  depends_on "python" => :recommended
+  depends_on "rsync" => :recommended
+  depends_on "vim" => :recommended
+  depends_on "watch" => :recommended
+  depends_on "wdiff" => :recommended
+  depends_on "wget" => :recommended
+  depends_on "zsh" => :recommended
 
   depends_on "gdb" => :optional
   depends_on "m4" => :optional
@@ -37,14 +37,13 @@ class ShinyAndGnu < Formula
   depends_on "unzip" => :optional
 
   def install
-    opoo 'Enjoy your GNU environment!'
+    opoo "Enjoy your GNU environment!"
   end
 
   def caveats; <<~EOS
     Commands also provided by macOS have been installed with the prefix "g".
     If you need to use these commands with their normal names, you
-    can add a directory to your PATH from your bashrc like:
-      PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+    can add a directory to your PATH.
   EOS
   end
 
